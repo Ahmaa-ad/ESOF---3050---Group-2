57bcec917833f8b22b0534cd8240131ab21aa1e4
Demetra <Demetra@LAPTOP-G6T1J1Q1.lakeheadu.ca>
2022-09-27 11:08:33 -0400

    Oy Vey.

--- ESOF 3050 Project/src/Main.java
+++ ESOF 3050 Project/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 		System.out.println("Hello world!");
 		System.out.println("Goodbye world!");
 		System.out.println("Taa-ddaaa!");
-		System.out.println("Oy Vey");
 		System.out.println("Oooooo");
 	}
 }
